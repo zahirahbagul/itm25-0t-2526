c1a034097fb05a1828df5e3d23b963f1d3b085cd
zahirahbagul <zahirah.bagul@student.ateneo.edu>
2025-06-19 21:39:36 +0800

    set_1.py

--- programming_sets/set_1.py
+++ programming_sets/set_1.py
@@ -30,7 +30,12 @@ def savings(gross_pay, tax_rate, expenses):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    
+def savings(gross_pay, tax_rate, expenses):
+    tax_deducted = tax_rate * gross_pay
+    take_home_pay = int(gross_pay - tax_deducted)
+    remaining = take_home_pay - expenses
+    return remaining
 
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''Material Waste.
@@ -64,7 +69,11 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    used = num_jobs * job_consumption
+    waste = total_material - used
+    return str(waste) + material_units
 
 def interest(principal, rate, periods):
     '''Interest.
@@ -93,4 +102,8 @@ def interest(principal, rate, periods):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+
+def interest(principal, rate, periods):
+    interest_earned = principal * rate * periods
+    final_value = int(principal + interest_earned)
+    return final_value

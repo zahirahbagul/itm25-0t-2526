8e28318e309465f6b83d40c42d4b7d2ce581133a
zahirahbagul <zahirah.bagul@student.ateneo.edu>
2025-06-26 16:39:51 +0800

    Added scytale_cipher

--- programming_sets/set_2.py
+++ programming_sets/set_2.py
@@ -64,54 +64,14 @@ def scytale_cipher(message, shift):
     '''Scytale Cipher.
 
     Encrypts a message by simulating a scytale cipher.
-
-    A scytale is a cylinder around which you can wrap a long strip of
-        parchment that contained a string of apparent gibberish. The parchment,
-        when read using the scytale, would reveal a message due to every nth
-        letter now appearing next to each other, revealing a proper message.
-    This encryption method is obsolete and should never be used to encrypt
-        data in production settings.
-
-    You may read more about the method here:
-        https://en.wikipedia.org/wiki/Scytale
-
-    You may follow this algorithm to implement a scytale-style cipher:
-    1. Take a message to be encoded and a "shift" number.
-        For this example, we will use "INFORMATION_AGE" as
-        the message and 3 as the shift.
-    2. Check if the length of the message is a multiple of
-        the shift. If it is not, add additional underscores
-        to the end of the message until it is.
-        In this example, "INFORMATION_AGE" is already a multiple of 3,
-        so we will leave it alone.
-    3. This is the tricky part. Construct the encoded message.
-        For each index i in the encoded message, use the character at the index
-        (i // shift) + (len(message) // shift) * (i % shift) of the raw message.
-        If this number doesn't make sense, you can play around with the cipher at
-         https://dencode.com/en/cipher/scytale to try to understand it.
-    4. Return the encoded message. In this case,
-        the output should be "IMNNA_FTAOIGROE".
-
-    Example:
-    scytale_cipher("INFORMATION_AGE", 3) -> "IMNNA_FTAOIGROE"
-    scytale_cipher("INFORMATION_AGE", 4) -> "IRIANMOGFANEOT__"
-    scytale_cipher("ALGORITHMS_ARE_IMPORTANT", 8) -> "AOTSRIOALRH_EMRNGIMA_PTT"
-
-    Parameters
-    ----------
-    message: str
-        a string of uppercase English letters and underscores (underscores represent spaces)
-    shift: int
-        a positive int that does not exceed the length of message
-
-    Returns
-    -------
-    str
-        the encoded message
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    while len(message) % shift != 0:
+        message += "_"
+    encoded= ""
+    for i in range(len(message)):
+        index = (i//shift) + (len(message)//shift) * (i%shift)
+        encoded += message[index]
+    return encoded
 
 def scytale_decipher(message, shift):
     '''Scytale De-cipher.

38786a0dc342fbad52c2d97b38122a6bf78495cd
zahirahbagul <zahirah.bagul@student.ateneo.edu>
2025-06-26 16:20:29 +0800

    added shift_by_letter

--- programming_sets/set_2.py
+++ programming_sets/set_2.py
@@ -38,30 +38,11 @@ def shift_by_letter(letter, letter_shift):
     '''Shift By Letter.
 
     Shift a letter to the right using the number equivalent of another letter.
-    The shift letter is any letter from A to Z, where A represents 0, B represents 1,
-        ..., Z represents 25.
-
-    Examples:
-    shift_by_letter("A", "A") -> "A"
-    shift_by_letter("A", "C") -> "C"
-    shift_by_letter("B", "K") -> "L"
-    shift_by_letter(" ", _) -> " "
-
-    Parameters
-    ----------
-    letter: str
-        a single uppercase English letter, or a space.
-    letter_shift: str
-        a single uppercase English letter.
-
-    Returns
-    -------
-    str
-        the letter, shifted appropriately.
     '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    if letter == " ":
+        return " "
+    shift = ord (letter_shift)-ord("A")
+    return shift_letter(letter, shift)
 
 def vigenere_cipher(message, key):
     '''Vigenere Cipher.

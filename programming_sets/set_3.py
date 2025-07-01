c02f1d87c761b8a0c923638e7a20818238f961c5
zahirahbagul <zahirah.bagul@student.ateneo.edu>
2025-07-01 22:44:34 +0800

    Update set_3.py

--- programming_sets/set_3.py
+++ programming_sets/set_3.py
@@ -1,101 +1,63 @@
-'''Programming Set 3
-
-This assignment will develop your ability to manipulate data.
-'''
-
 def relationship_status(from_member, to_member, social_graph):
-    '''Relationship Status.
+    follows_from = social_graph[from_member]["following"]
+    follows_to = social_graph[to_member]["following"]
 
-    Let us pretend that you are building a new app.
-    Your app supports social media functionality, which means that users can have
-    relationships with other users.
+    both_follow = from_member in follows_to and to_member in follows_from
+    only_from = to_member in follows_from
+    only_to = from_member in follows_to
 
-    There are two guidelines for describing relationships on this social media app:
-    1. Any user can follow any other user.
-    2. If two users follow each other, they are considered friends.
-
-    This function describes the relationship that two users have with each other.
-
-    Please see "assignment-4-sample-data.py" for sample data. The social graph
-    will adhere to the same pattern.
-
-    Parameters
-    ----------
-    from_member: str
-        the subject member
-    to_member: str
-        the object member
-    social_graph: dict
-        the relationship data
-
-    Returns
-    -------
-    str
-        "follower" if fromMember follows toMember,
-        "followed by" if fromMember is followed by toMember,
-        "friends" if fromMember and toMember follow each other,
-        "no relationship" if neither fromMember nor toMember follow each other.
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
-
+    if both_follow:
+        return "friends"
+    elif only_to:
+        return "followed by"
+    elif only_from:
+        return "follower"
+    else:
+        return "no relationship"
 
 def tic_tac_toe(board):
-    '''Tic Tac Toe.
+    board_size = len(board)
+    consecutive = 0
 
-    Tic Tac Toe is a common paper-and-pencil game.
-    Players must attempt to successfully draw a straight line of their symbol across a grid.
-    The player that does this first is considered the winner.
+    for row in range(board_size):
+        consecutive = 0
+        for col in range(board_size - 1):
+            if board[row][col] == board[row][col + 1] and board[row][col] != "":
+                consecutive += 1
+            else:
+                break
+        if consecutive == board_size - 1:
+            return board[row][0]
 
-    This function evaluates a tic tac toe board and returns the winner.
+    for col in range(board_size):
+        consecutive = 0
+        for row in range(board_size - 1):
+            if board[row][col] == board[row + 1][col] and board[row][col] != "":
+                consecutive += 1
+            else:
+                break
+        if consecutive == board_size - 1:
+            return board[0][col]
 
-    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
-    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
-    have more than one winner. The board will only ever have 2 unique symbols at the same time.
-
-    Parameters
-    ----------
-    board: list
-        the representation of the tic-tac-toe board as a square list of lists
-
-    Returns
-    -------
-    str
-        the symbol of the winner or "NO WINNER" if there is no winner
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    consecutive = 0
+    for index in range(board_size - 1):
+        if board[index][index] == board[index + 1][index + 1] and board[index][index] != "":
+            consecutive += 1
+        else:
+            break
+    if consecutive == board_size - 1:
+        return board[0][0]
+        return "NO WINNER"
 
 def eta(first_stop, second_stop, route_map):
-    '''ETA.
+    total_time = 0
+    current_stop = first_stop
 
-    A shuttle van service is tasked to travel along a predefined circlar route.
-    This route is divided into several legs between stops.
-    The route is one-way only, and it is fully connected to itself.
+    while current_stop != second_stop:
+        for segment in route_map:
+            if segment[0] == current_stop:
+                total_time += route_map[segment]["travel_time_mins"]
+                current_stop = segment[1]
+                break
 
-    This function returns how long it will take the shuttle to arrive at a stop
-    after leaving another stop.
-
-    Please see "mod-4-ipa-1-sample-data.py" for sample data. The route map will
-    adhere to the same pattern. The route map may contain more legs and more stops,
-    but it will always be one-way and fully enclosed.
-
-    Parameters
-    ----------
-    first_stop: str
-        the stop that the shuttle will leave
-    second_stop: str
-        the stop that the shuttle will arrive at
-    route_map: dict
-        the data describing the routes
-
-    Returns
-    -------
-    int
-        the time it will take the shuttle to travel from first_stop to second_stop
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    return total_time
